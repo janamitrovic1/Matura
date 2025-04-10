4f0d474fa0146f50ebc118f3ee20a01b38e7d2fe
Jana Mitrović <116366696+janamitrovic2@users.noreply.github.com>
2025-04-10 15:19:40 +0200

    1.0.0

---
+++ B1/components/nav.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="header">
+            <div>
+                <h1>Фото Галерија</h1>
+            </div>
+        </div>
+        <nav>
+            <div class="links">
+                <a href=".">Početna</a>
+                <a href="">O autoru</a>
+                <a href="">Uputstvo</a>
+                <span>Zavrsni ispit</span>
+            </div>
+            
+        </nav>
+    </body>
+</html>
