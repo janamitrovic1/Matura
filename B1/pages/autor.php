fa5e36a6256c30dce89b8627a5b122352a336752
Jana Mitrovic <mitrovicjanakv@gmail.com>
2025-04-27 21:33:24 +0200

    dodat autor

--- B1/pages/autor.php
+++ B1/pages/autor.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/globals.css">
     <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="../css/autor.css">
     <title>Autor</title>
 </head>
 <body>
@@ -12,8 +13,12 @@
         $url = "Фото галерија - аутор";
         require_once '../components/nav.php'; 
     ?>
-    <div>
+    <div class="autortext">
+        <h1>Jaна Митровић</h1>
+
         <p>Autor ove stranice je Jana Mitrović. Učenik četvrtog razreda smera elektrotehničar informacionih tehnologija.</p>
+        <pre>gmail: mitrovicjanakv@gmail.com
+        </pre>
     </div>
 </body>
 </html>
