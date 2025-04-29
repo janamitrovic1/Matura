c6dec0c8632f18d28f3e3ca2d9a2490a0a19f098
Jana Mitrovic <mitrovicjanakv@gmail.com>
2025-04-29 23:04:32 +0200

    dodat author page

--- B2/pages/autor.php
+++ B2/pages/autor.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/globals.css">
     <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="../css/autor.css">
     <title>Аутор</title>
 </head>
 <body>
@@ -13,5 +14,13 @@
         $naslov="Домаће животиње - аутор";
         require_once("../components/header.php");
     ?>
+    <div>
+        <h1>Аутор: Јана Митровић</h1>
+        <h3>Gmail: <a target="_blank" href="mailto:someone@example.com">mitrovicjanakv@gmail.com</a></h3>
+        <h3>GitHub: <a target="_blank" href="https://github.com/janamitrovic1">https://github.com/janamitrovic1</a></h3>
+        <p>ЕСТШ "Никола Тесла" Краљево</p>
+        <p>Практични рад за матурски испит.</p>
+    </div>
+
 </body>
 </html>
