039cb1580936937307f7450b62e4e567f2f0dc3d
Jana Mitrovic <mitrovicjanakv@gmail.com>
2025-05-07 21:06:37 +0200

    kreirane stranice

---
+++ B8/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="sr-RS">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/globals.css">
+    <title>Књига утисака</title>
+</head>
+<body>
+    <?php
+        require_once("./components/nav.php");
+    ?>
+    <?php
+        require_once("./components/footer.php");
+    ?>
+</body>
+</html>
