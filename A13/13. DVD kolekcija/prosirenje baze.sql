2e7e9b70002f5af499c9913400bbefb7213c65ff
Jana Mitrovic <mitrovicjanakv@gmail.com>
2025-05-14 08:35:35 +0200

    Dodano: prosirenje baze.sql

---
+++ A13/13. DVD kolekcija/prosirenje baze.sql
@@ -0,0 +1,21 @@
+﻿ALTER table GLUMAC ADD  
+Nadimak varchar(30);
+
+CREATE TABLE PRIMERAK(
+PrimerakID int primary key,
+FilmID int references FILM(filmid)
+);
+CREATE TABLE ZAPOSLENI(
+ZaposleniID int primary key,
+ime varchar(30) not null,
+prezime varchar(30) not null,
+telefon varchar(30) not null,
+adresa varchar(30) not null
+);
+CREATE TABLE POZAJMICA(
+PrimerakID int references PRIMERAK(PrimerakID),
+ZaposleniID int references ZAPOSLENI(ZaposleniID),
+Datum_pozajmice DATE not null,
+Datum_vracanja DATE,
+PRIMARY KEY(PrimerakID,ZaposleniID,Datum_pozajmice)
+);
