33111038bf3cdb80d98b37572e5e02c691e21248
Jana Mitrović <116366696+janamitrovic2@users.noreply.github.com>
2025-05-14 12:12:52 +0200

    zavrsen autor i uputstvo

--- B8/autor.php
+++ B8/autor.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>О аутору</title>
+    <link rel="stylesheet" href="./css/globals.css">
+    <link rel="stylesheet" href="./css/autor.css">
 </head>
 <body>
+    <?php
+    require_once("./components/nav.php");
+    ?>
+    <div class="main">
+        <h1>О аутору</h1>
+        <p>Моје име је Jaна Митровић. Ученик сам Електро-саобраћајно техничке школе "Никола Тесла" у Краљеву.</p>
+        <p>Ово је задатак за матурски испит.</p>
+        <p>У слободно време волим да читам књиге, тренирам итд.</p>
+    </div>
     
+    <?php 
+    require_once("./components/footer.php");
+    ?>
 </body>
 </html>
--- B8/uputstvo.php
+++ B8/uputstvo.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Упутство</title>
+    <link rel="stylesheet" href="./css/globals.css">
+    <link rel="stylesheet" href="./css/autor.css">
 </head>
 <body>
-    
+    <?php
+    require_once("./components/nav.php");
+    ?>
+    <div class="main">
+        <h1>Упутство</h1>
+        <p>Након што унесете све податке у поља за унос и имејл адресу у исправном облику можете кликом на 
+            дугме "Додај коментар" послати ваш коментар.
+        </p>
+    </div>
+    <?php 
+    require_once("./components/footer.php");
+    ?>
 </body>
 </html>
