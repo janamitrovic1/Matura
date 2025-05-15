c526abf20dcf22f7f993b30c3b2753c20036c372
Jana Mitrović <116366696+janamitrovic2@users.noreply.github.com>
2025-05-15 12:05:14 +0200

    nav i footer

---
+++ B10/components/footer.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <div class="footer">
+        &copy Osnovna škola "Sonja Marinkovic"
+    </div>
+
+</body>
+</html>
---
+++ B10/components/nav.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="rs-SR">
+<body>
+    <div>
+        <div class="header">
+            <h1>Elektronski recnik</h1>
+        </div>
+        <div class="nav">
+            <a href=".">Recnik</a>
+            <a href="./add.php">Dodavanje novih reci</a>
+            <a href="./uputstvo.php">Uputstvo</a>
+        </di>
+    </div>
+</body>
+</html>
