542c69f70d4ebdc2e2b0bb10e462db362977ce27
Jana Mitrovic <mitrovicjanakv@gmail.com>
2025-06-17 18:41:59 +0200

    teorija

---
+++ B4/components/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <div class="header">
+        <p>OSNOVNA SKOLA "SONJA MARINKOVIC"</p>
+    </div>
+    <div class="nav">
+        <div>
+            <a href=".">Pocetna</a>
+        </div>
+        <div>
+            <a href="../autor.php">O autoru</a>
+        </div>
+        <div>
+            <a href="../uputstvo.php">Uputstvo</a>
+        </div>
+    </div>  
+</body>
+</html>
---
+++ B4/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/globals.css" class="rel">
+    <link rel="stylesheet" href="css/main.css" class="rel">
+    <title>Pikaso</title>
+</head>
+<body>
+    <?php
+        require_once("components/header.php");
+    ?>
+
+    <main>
+        <div class="levi">
+            <p>PIKASO</p>
+        </div>
+        <div>
+
+        </div>
+
+    </main>
+</body>
+</html>
